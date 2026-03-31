e537674ed29d82f0235ea8e7bd259bdc3fa7d73b
Jonas Wüpping <j.wuepping@evosec.de>
2026-03-31 10:44:22 +0200

    Initial commit

---
+++ src/test/java/com/example/buildpackextensionexample/BuildpackExtensionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildpackextensionexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildpackExtensionExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
